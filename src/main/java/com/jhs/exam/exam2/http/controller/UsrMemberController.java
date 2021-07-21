efa1abd6c130018a234fdad715e5cbca4acfd78c
sungbum1004 <tlsqkfka96@naver.com>
2021-07-21 13:16:17 +0900

    로그인 실패 후 액션 종료 안되는 버그 수정

--- src/main/java/com/jhs/exam/exam2/http/controller/UsrMemberController.java
+++ src/main/java/com/jhs/exam/exam2/http/controller/UsrMemberController.java
@@ -51,6 +51,7 @@ public class UsrMemberController extends Controller {
 
 		if (loginRd.isFail()) {
 			rq.historyBack(loginRd.getMsg());
+			return;
 		}
 
 		Member member = (Member) loginRd.getBody().get("member");
