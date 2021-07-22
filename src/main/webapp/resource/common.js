c55179bdcf8e568f1ea41746196910a3f11a1c92
sungbum1004 <tlsqkfka96@naver.com>
2021-07-22 11:15:34 +0900

    검색바 적용

--- src/main/webapp/resource/common.js
+++ src/main/webapp/resource/common.js
@@ -1,27 +1,60 @@
 console.log("common.js 로딩");
 
+/* 모바일 탑바 시작 */
 function MobileTopBar__init() {
-  $('.btn-show-mobile-side-bar').click(function() {
-    MobileSideBar__show();
-  });
+	$('.mobile-top-bar .btn-show-mobile-side-bar').click(function() {
+		MobileSideBar__show();
+	});
 }
+/* 모바일 탑바 끝 */
 
+/* 모바일 사이드바 시작 */
 function MobileSideBar__init() {
-  $('.btn-close-mobile-side-bar').click(function() {
-    MobileSideBar__hide();
-  })
+	$('.mobile-side-bar .btn-close-mobile-side-bar').click(function() {
+		MobileSideBar__hide();
+	})
 }
 
 function MobileSideBar__show() {
-  $('.mobile-side-bar').addClass('active');
-  $('html').addClass('mobile-side-bar-actived');
+	$('.mobile-side-bar').addClass('active');
+	$('html').addClass('mobile-side-bar-actived');
 }
 
 function MobileSideBar__hide() {
-  $('.mobile-side-bar').removeClass('active');
-  $('html').removeClass('mobile-side-bar-actived');
+	$('.mobile-side-bar').removeClass('active');
+	$('html').removeClass('mobile-side-bar-actived');
+}
+/* 모바일 사이드바 끝 */
+
+/* 탑바 시작 */
+function TopBar__init() {
+  $('.top-bar .btn-show-search-bar').click(function() {
+    SearchBar__show();
+  });
+}
+/* 탑바 끝 */
+
+/* 검색바 시작 */
+function SearchBar__init() {
+  $('.search-bar .btn-hide-search-bar').click(function() {
+    SearchBar__hide();
+  });
 }
 
-MobileTopBar__init();
+function SearchBar__show() {
+  $('html').addClass('search-bar-actived');
+  $('.search-bar').addClass('active');
+  $('.search-bar form input[name="searchKeyword"]').focus();
+}
 
-MobileSideBar__init(); 
+function SearchBar__hide() {
+  $('html').removeClass('search-bar-actived');
+  $('.search-bar').removeClass('active');
+  $('.search-bar form input[name="searchKeyword"]').val('');
+}
+/* 검색바 끝 */
+
+MobileTopBar__init();
+MobileSideBar__init();
+TopBar__init();
+SearchBar__init(); 
