95f0bb6a7029b7f2bd3c5246fd4b8ff93716baef
sungbum1004 <tlsqkfka96@naver.com>
2021-07-28 10:41:28 +0900

    검색박스 꾸미기

--- src/main/webapp/resource/common.js
+++ src/main/webapp/resource/common.js
@@ -2,41 +2,41 @@ console.log("common.js 로딩");
 
 /* 모바일 탑바 시작 */
 function MobileTopBar__init() {
-	$('.mobile-top-bar .btn-show-mobile-side-bar').click(function() {
-		MobileSideBar__show();
-	});
+  $('.mobile-top-bar .btn-show-mobile-side-bar').click(function() {
+    MobileSideBar__show();
+  });
+  
+  $('.mobile-top-bar .btn-show-search-bar').click(function() {
+    SearchBar__show();
+  });
 }
 /* 모바일 탑바 끝 */
 
 /* 모바일 사이드바 시작 */
 function MobileSideBar__init() {
-	$('.mobile-side-bar .btn-close-mobile-side-bar').click(function() {
-		MobileSideBar__hide();
-	});
-	
-	 $('.mobile-side-bar .btn-show-search-bar').click(function() {
+  $('.mobile-side-bar .btn-close-mobile-side-bar').click(function() {
+    MobileSideBar__hide();
+  })
+  
+  $('.mobile-side-bar .btn-show-search-bar').click(function() {
     SearchBar__show();
   });
 }
 
 function MobileSideBar__show() {
-	$('.mobile-side-bar').addClass('active');
-	$('html').addClass('mobile-side-bar-actived');
+  $('.mobile-side-bar').addClass('active');
+  $('html').addClass('mobile-side-bar-actived');
 }
 
 function MobileSideBar__hide() {
-	$('.mobile-side-bar').removeClass('active');
-	$('html').removeClass('mobile-side-bar-actived');
+  $('.mobile-side-bar').removeClass('active');
+  $('html').removeClass('mobile-side-bar-actived');
 }
 /* 모바일 사이드바 끝 */
 
 /* 탑바 시작 */
-function TopBar__init() {
+function TopBar__init() {  
   $('.top-bar .btn-show-search-bar').click(function() {
-    SearchBar__show();
-  });
-  
-  $('.mobile-top-bar .btn-show-search-bar').click(function() {
     SearchBar__show();
   });
 }
@@ -52,7 +52,10 @@ function SearchBar__init() {
 function SearchBar__show() {
   $('html').addClass('search-bar-actived');
   $('.search-bar').addClass('active');
-  $('.search-bar form input[name="searchKeyword"]').focus();
+  
+  setTimeout(function() {
+	$('.search-bar form input[name="searchKeyword"]').focus();
+  }, 100);
 }
 
 function SearchBar__hide() {
@@ -65,4 +68,4 @@ function SearchBar__hide() {
 MobileTopBar__init();
 MobileSideBar__init();
 TopBar__init();
-SearchBar__init(); 
+SearchBar__init();
